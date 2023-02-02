9b993f64c0326b0696424353a99b4d70b01c41d5
Ionnier <dragosbahrim@outlook.com>
2023-02-02 23:03:47 +0200

    B

--- front/lib/screens/agenda/AgendaScreen.dart
+++ front/lib/screens/agenda/AgendaScreen.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:add_2_calendar/add_2_calendar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:front/data/task_local_repository.dart';
 import 'package:front/main.dart';
 import 'package:front/screens/agenda/widgets/AgendaWidget.dart';
