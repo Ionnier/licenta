2c4b1ecdf8744fdffa3855167b5670cb57c1335a
Ionnier <dragosbahrim@outlook.com>
2023-02-18 00:18:45 +0200

    M

--- front/lib/screens/TaskDetailsScreen.dart
+++ front/lib/screens/TaskDetailsScreen.dart
@@ -142,168 +142,151 @@ class _TaskDetailsScreenState extends State<TaskDetailsScreen> {
             Wrap(
               spacing: 8.0,
               children: [
-                Expanded(
-                    child: FilledButton(
-                        onPressed: () async {
-                          final _formKey = GlobalKey<FormState>();
-                          final commentController = TextEditingController();
-                          final timeSpentController = TextEditingController();
-                          final Activity activity = Activity();
-                          var result = await showDialog<bool>(
-                            context: context,
-                            builder: (BuildContext context) => AlertDialog(
-                              title: const Text('Add work'),
-                              content: Form(
-                                  key: _formKey,
-                                  child: ListView(children: [
-                                    TextFormField(
-                                      decoration: const InputDecoration(
-                                        border: OutlineInputBorder(),
-                                        hintText: 'Comment',
-                                      ),
-                                      controller: commentController,
-                                    ),
-                                    const SizedBox(
-                                      height: 4.0,
-                                    ),
-                                    TextFormField(
-                                        decoration: const InputDecoration(
-                                          border: OutlineInputBorder(),
-                                          hintText: 'Time spent',
-                                        ),
-                                        validator: (value) {
-                                          if (value == null) {
-                                            return "Can't be empty";
-                                          }
-                                          var parse = int.tryParse(value);
-                                          if (parse == null || parse == 0) {
-                                            return "Can't be non number";
-                                          }
-                                        },
-                                        readOnly: true,
-                                        controller: timeSpentController,
-                                        onTap: () {
-                                          var date = DateTime.now();
-                                          DatePicker.showTimePicker(context,
-                                              showTitleActions: true,
-                                              currentTime: DateTime(date.year,
-                                                      date.month, date.day)
-                                                  .add(Duration(
-                                                      milliseconds: int.tryParse(
-                                                              timeSpentController
-                                                                  .text) ??
-                                                          0)),
-                                              onConfirm: (date) {
-                                            timeSpentController.text = date
-                                                .difference(DateTime(date.year,
-                                                    date.month, date.day))
-                                                .inMilliseconds
-                                                .toString();
-                                          });
-                                        }),
-                                    const SizedBox(
-                                      height: 4.0,
-                                    ),
-                                  ])),
-                              actions: <Widget>[
-                                TextButton(
-                                  onPressed: () =>
-                                      Navigator.pop(context, false),
-                                  child: const Text('Cancel'),
+                FilledButton(
+                    onPressed: () async {
+                      final _formKey = GlobalKey<FormState>();
+                      final commentController = TextEditingController();
+                      final timeSpentController = TextEditingController();
+                      final Activity activity = Activity();
+                      var result = await showDialog<bool>(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                          title: const Text('Add work'),
+                          content: Form(
+                              key: _formKey,
+                              child: ListView(children: [
+                                TextFormField(
+                                  decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Comment',
+                                  ),
+                                  controller: commentController,
                                 ),
-                                TextButton(
-                                  onPressed: () {
-                                    if (!_formKey.currentState!.validate()) {
-                                      return;
-                                    } else {
-                                      if (commentController.text.isNotEmpty) {
-                                        activity.comment =
-                                            commentController.text;
+                                const SizedBox(
+                                  height: 4.0,
+                                ),
+                                TextFormField(
+                                    decoration: const InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      hintText: 'Time spent',
+                                    ),
+                                    validator: (value) {
+                                      if (value == null) {
+                                        return "Can't be empty";
                                       }
-                                      activity.timeSpent = int.tryParse(
-                                          timeSpentController.text)!;
-                                      activity.parentId = task.localId;
-                                      Navigator.pop(context, true);
-                                    }
-                                  },
-                                  child: const Text('OK'),
+                                      var parse = int.tryParse(value);
+                                      if (parse == null || parse == 0) {
+                                        return "Can't be non number";
+                                      }
+                                    },
+                                    readOnly: true,
+                                    controller: timeSpentController,
+                                    onTap: () {
+                                      var date = DateTime.now();
+                                      DatePicker.showTimePicker(context,
+                                          showTitleActions: true,
+                                          currentTime: DateTime(date.year,
+                                                  date.month, date.day)
+                                              .add(Duration(
+                                                  milliseconds: int.tryParse(
+                                                          timeSpentController
+                                                              .text) ??
+                                                      0)), onConfirm: (date) {
+                                        timeSpentController.text = date
+                                            .difference(DateTime(date.year,
+                                                date.month, date.day))
+                                            .inMilliseconds
+                                            .toString();
+                                      });
+                                    }),
+                                const SizedBox(
+                                  height: 4.0,
                                 ),
-                              ],
+                              ])),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () => Navigator.pop(context, false),
+                              child: const Text('Cancel'),
                             ),
-                          );
-                          if (result == true) {
-                            await LocalTaskDbRepository()
-                                .insertActivity(activity);
-                            updateTask();
-                          }
-                        },
-                        child: const Text("Add work"))),
-                Expanded(
-                    child: FilledButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => InputTaskScreen(
-                                      activityType: task.isHabit()
-                                          ? habitType
-                                          : activityType,
-                                      task: task,
-                                    )),
-                          );
-                        },
-                        child: const Text("Edit"))),
-                const SizedBox(
-                  width: 8.0,
-                ),
-                Expanded(
-                    child: FilledButton(
-                        onPressed: () async {
-                          var result = await showDialog<bool>(
-                            context: context,
-                            builder: (BuildContext context) => AlertDialog(
-                              title: const Text('Delete task'),
-                              content: const Text(
-                                  'Are you sure you want to delete this task?'),
-                              actions: <Widget>[
-                                TextButton(
-                                  onPressed: () =>
-                                      Navigator.pop(context, false),
-                                  child: const Text('Cancel'),
-                                ),
-                                TextButton(
-                                  onPressed: () => Navigator.pop(context, true),
-                                  child: const Text('OK'),
-                                ),
-                              ],
+                            TextButton(
+                              onPressed: () {
+                                if (!_formKey.currentState!.validate()) {
+                                  return;
+                                } else {
+                                  if (commentController.text.isNotEmpty) {
+                                    activity.comment = commentController.text;
+                                  }
+                                  activity.timeSpent =
+                                      int.tryParse(timeSpentController.text)!;
+                                  activity.parentId = task.localId;
+                                  Navigator.pop(context, true);
+                                }
+                              },
+                              child: const Text('OK'),
                             ),
-                          );
-                          if (result == true) {
-                            await LocalTaskDbRepository().deleteTask(task);
-                            if (context.mounted) Navigator.pop(context);
-                          }
-                        },
-                        child: const Text("Delete"))),
-                const SizedBox(
-                  width: 8.0,
-                ),
+                          ],
+                        ),
+                      );
+                      if (result == true) {
+                        await LocalTaskDbRepository().insertActivity(activity);
+                        updateTask();
+                      }
+                    },
+                    child: const Text("Add work")),
+                FilledButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => InputTaskScreen(
+                                  activityType:
+                                      task.isHabit() ? habitType : activityType,
+                                  task: task,
+                                )),
+                      );
+                    },
+                    child: const Text("Edit")),
+                FilledButton(
+                    onPressed: () async {
+                      var result = await showDialog<bool>(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                          title: const Text('Delete task'),
+                          content: const Text(
+                              'Are you sure you want to delete this task?'),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () => Navigator.pop(context, false),
+                              child: const Text('Cancel'),
+                            ),
+                            TextButton(
+                              onPressed: () => Navigator.pop(context, true),
+                              child: const Text('OK'),
+                            ),
+                          ],
+                        ),
+                      );
+                      if (result == true) {
+                        await LocalTaskDbRepository().deleteTask(task);
+                        if (context.mounted) Navigator.pop(context);
+                      }
+                    },
+                    child: const Text("Delete")),
                 if (!task.isHabit() && task.parentId == null)
-                  Expanded(
-                      child: FilledButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => InputTaskScreen(
-                                        activityType: activityType,
-                                        parentId: task.localId,
-                                      )),
-                            );
-                          },
-                          child: const Text("Add subtask"))),
+                  FilledButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => InputTaskScreen(
+                                    activityType: activityType,
+                                    parentId: task.localId,
+                                  )),
+                        );
+                      },
+                      child: const Text("Add subtask")),
                 if (!task.isHabit())
-                  Expanded(
-                      child: FilledButton(
+                  FilledButton(
                     onPressed: () async {
                       await LocalTaskDbRepository().markTaskAsCompleted(task);
                       if (context.mounted) {
@@ -311,7 +294,7 @@ class _TaskDetailsScreenState extends State<TaskDetailsScreen> {
                       }
                     },
                     child: const Text("Mark as completed"),
-                  ))
+                  )
               ],
             ),
             const SizedBox(height: 16.0),
